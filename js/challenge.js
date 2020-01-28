4c3c06cdc2dac38249af4b8ce7cfb6b24f99435a
William Barela <william.barela.dev@gmail.com>
2020-01-28 05:56:28 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', app(event));
 
 function countUp(counter){
-  counter.innerText = `${parseInt(counter.innerText) + 1}`;
+  return `${parseInt(counter.innerText) + 1}`;
 }
 
 function app(event){
   let counter = document.getElementById('counter');
   while(true){
     setTimeout(countUp(counter), 1000);
-  }
+}
 }
